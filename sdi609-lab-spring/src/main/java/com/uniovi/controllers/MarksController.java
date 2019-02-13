c1c56db771be87e7e307e2e43143e9decdef9ade
Cristina Ruiz de Bucesta <cristinaruizdebucesta@gmail.com>
2019-02-13 20:49:26 +0100

    SDI - 1.9 Beans - Servicios, inyección de dependencias.

---
+++ sdi609-lab-spring/src/main/java/com/uniovi/controllers/MarksController.java
@@ -0,0 +1,37 @@
+package com.uniovi.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import com.uniovi.entities.Mark;
+import com.uniovi.services.MarksService;
+
+@RestController
+public class MarksController {
+
+	@Autowired // Inyectar el servicio
+	private MarksService marksService;
+
+	@RequestMapping("/mark/list")
+	public String getList() {
+		return marksService.getMarks().toString();
+	}
+
+	@RequestMapping(value = "/mark/add", method = RequestMethod.POST)
+	public String setMark(@ModelAttribute Mark mark) {
+		marksService.addMark(mark);
+		return "Ok";
+	}
+
+	@RequestMapping("/mark/details/{id}")
+	public String getDetail(@PathVariable Long id) {
+		return " Getting Detail: " + id;
+	}
+
+	@RequestMapping("/mark/delete/{id}")
+	public String deleteMark(@PathVariable Long id) {
+		marksService.deleteMark(id);
+		return "Ok";
+	}
+
+}
--- sdi609-lab-spring/src/main/java/com/uniovi/entities/Mark.java
+++ sdi609-lab-spring/src/main/java/com/uniovi/entities/Mark.java
@@ -5,22 +5,43 @@ public class Mark {
 	private String description;
 	private Double score;
 
+	public Mark(Long id, String description, Double score) {
+		super();
+		this.id = id;
+		this.description = description;
+		this.score = score;
+	}
+
+	public Mark() {
+	}
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
-	this.id = id;
+		this.id = id;
 	}
+
 	public String getDescription() {
-	return description;
+		return description;
 	}
+
 	public void setDescription(String description) {
-	this.description = description;
+		this.description = description;
 	}
+
 	public Double getScore() {
-	return score;
+		return score;
 	}
+
 	public void setScore(Double score) {
-	this.score = score;
+		this.score = score;
 	}
+	
+	@Override
+	public String toString() {
+	return "Mark [id=" + id + ", description=" + description + ", score=" + score + "]";
+	}
+
 }
