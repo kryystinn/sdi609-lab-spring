b43cc70e3c6b530967b41aa049e43d14d56bbaed
Cristina Ruiz de Bucesta <cristinaruizdebucesta@gmail.com>
2019-02-16 07:48:57 +0100

    SDI -SpringBoot-2 - Thymeleaf-fragmentos.

--- sdi609-lab-spring/src/main/java/com/uniovi/controllers/MarksController.java
+++ sdi609-lab-spring/src/main/java/com/uniovi/controllers/MarksController.java
@@ -21,6 +21,12 @@ public class MarksController {
 
 	}
 
+	@RequestMapping("/mark/list/update")
+	public String updateList(Model model) {
+		model.addAttribute("markList", marksService.getMarks());
+		return "mark/list :: tableMarks";
+	}
+
 	@RequestMapping(value = "/mark/add")
 	public String getMark() {
 		return "mark/add";
