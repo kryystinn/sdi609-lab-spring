3641594dd99857b3e6e7288bc49deaf933ae7387
Cristina Ruiz de Bucesta <cristinaruizdebucesta@gmail.com>
2019-02-18 14:01:28 +0100

    SDI-SpringBoot- 5 - Autenticación y control de acceso

--- sdi609-lab-spring/src/main/java/com/uniovi/entities/User.java
+++ sdi609-lab-spring/src/main/java/com/uniovi/entities/User.java
@@ -12,6 +12,9 @@ public class User {
 	private String dni;
 	private String name;
 	private String lastName;
+	private String password;
+	@Transient // propiedad que no se almacena e la tabla.
+	private String passwordConfirm;
 	private String role;
 	@OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
 	private Set<Mark> marks;
@@ -68,5 +71,21 @@ public class User {
 
 	public String getFullName() {
 		return this.name + " " + this.lastName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getPasswordConfirm() {
+		return passwordConfirm;
+	}
+
+	public void setPasswordConfirm(String passwordConfirm) {
+		this.passwordConfirm = passwordConfirm;
 	}
 }
