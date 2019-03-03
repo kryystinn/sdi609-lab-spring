11ae3fc3b5d05a27c9fd02e63a60d6ae4f80b832
Cristina Ruiz de Bucesta <cristinaruizdebucesta@gmail.com>
2019-03-03 19:38:06 +0100

    SDI-Selenium-5.4-CasosUso-VistaHome

--- src/test/java/com/uniovi/tests/NotaneitorTests.java
+++ src/test/java/com/uniovi/tests/NotaneitorTests.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 import com.uniovi.tests.pageobjects.PO_HomeView;
+import com.uniovi.tests.pageobjects.PO_Properties;
 import com.uniovi.tests.util.SeleniumUtils;
 
 //Ordenamos las pruebas por el nombre del método
@@ -54,12 +55,50 @@ public class NotaneitorTests {
 		driver.quit();
 	}
 
-	@Test
-	public void test() {
+//	@Test
+//	public void test() {
 //		//Cambiamos el idioma a Inglés
 //		PO_HomeView.changeIdiom(driver, "btnEnglish");
 //		//Esperamos porque aparezca que aparezca el texto de bienvenida en inglés
 //		SeleniumUtils.EsperaCargaPagina(driver, "text", p.getString("welcome.message",
 //				PO_Properties.ENGLISH), getTimeout());
+//	}
+	
+	
+	//PR01. Acceder a la página principal /
+	@Test
+	public void PR01() {
+		PO_HomeView.checkWelcome(driver, PO_Properties.getSPANISH());
 	}
+
+	//PR02. OPción de navegación. Pinchar en el enlace Registro en la página home
+	@Test
+	public void PR02() {
+		PO_HomeView.clickOption(driver, "signup", "class", "btn btn-primary");
+	}
+
+	//PR03. OPción de navegación. Pinchar en el enlace Identificate en la página home
+	@Test
+	public void PR03() {
+		PO_HomeView.clickOption(driver, "login", "class", "btn btn-primary");
+	}
+
+	//PR04. OPción de navegación. Cambio de idioma de Español a Ingles y vuelta a Español
+	@Test
+	public void PR04() {
+		PO_HomeView.checkChangeIdiom(driver, "btnSpanish", "btnEnglish",
+				PO_Properties.getSPANISH(), PO_Properties.getENGLISH());
+		//SeleniumUtils.esperarSegundos(driver, 2);
+	}
+
+
+
+
+	
+	
+	
+	
+	
+	
+	
 }
